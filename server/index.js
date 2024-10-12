ebba51cc8aca17c27e1da572df969bc4b2a0be1c
AaronKurian <aaronkurianabraham@gmail.com>
2024-10-12 12:12:13 +0530

    3rd

--- server/index.js
+++ server/index.js
@@ -7,13 +7,10 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts',postRoutes);
-
-
 app.use(bodyParser.json({ limit: "30mb", exxtended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", exxtended: true}));
 app.use(cors());
-
+app.use('/posts',postRoutes);
 const CONNECTION_URL = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5000;
 
